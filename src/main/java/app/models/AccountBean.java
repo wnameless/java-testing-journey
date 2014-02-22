1f824fe8a3c0a34ec1a42a128133c92de795a29d
WMW <wnameless@gmail.com>
2014-02-22 17:16:27 -0500

    Add final to equalsTo & hashCode

--- src/main/java/app/models/AccountBean.java
+++ src/main/java/app/models/AccountBean.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * @author Wei-Ming Wu
+ *
+ *
+ * Copyright 2014 Wei-Ming Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
 package app.models;
 
 import java.util.List;
@@ -73,7 +93,7 @@ public class AccountBean implements Account {
   }
 
   @Override
-  public boolean equals(Object o) {
+  public final boolean equals(Object o) {
     if (o instanceof Account) {
       Account account = (Account) o;
       return Objects.equal(accountNumber, account.getAccountNumber())
@@ -84,7 +104,7 @@ public class AccountBean implements Account {
   }
 
   @Override
-  public int hashCode() {
+  public final int hashCode() {
     return Objects.hashCode(accountNumber, routingNumber, owners);
   }
 
